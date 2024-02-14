1431cff776cd94fa1834070a4d1473ef527abf53
Joe Corall <jjc223@lehigh.edu>
2024-02-14 12:57:35 -0500

    Print links to MODS URL and only count elements with values

--- 040-i7-metadata-audit/find_mods_paths.py
+++ 040-i7-metadata-audit/find_mods_paths.py
@@ -6,16 +6,16 @@ def print_elements(element, parent_path="", seen_paths={}, element_samples={}, f
     tag = element.tag[element.tag.find("}")+1:] if "}" in element.tag else element.tag
     current_path = f"{parent_path}/{tag}" if parent_path else tag
     getty_pattern = re.compile(r"http://vocab.getty.edu/page/aat/\d+")
-    
-    # Update occurrences for elements within a single file
-    file_occurrences[current_path] = file_occurrences.get(current_path, 0) + 1
 
-    # Handle element sample value
-    if current_path not in element_samples and element.text and element.text.strip():
-        element_samples[current_path] = {
-            'value': element.text.strip(),
-            'filename': current_filename
-        }
+    if element.text and element.text.strip():
+        # Update occurrences for elements within a single file
+        file_occurrences[current_path] = file_occurrences.get(current_path, 0) + 1
+        # Handle element sample value
+        if current_path not in element_samples:
+            element_samples[current_path] = {
+                'value': element.text.strip(),
+                'filename': current_filename
+            }
 
     # Update global occurrences for elements
     if current_path in seen_paths:
@@ -34,7 +34,8 @@ def print_elements(element, parent_path="", seen_paths={}, element_samples={}, f
         if "http://vocab.getty.edu/page/aat" in value:
             value = re.sub(getty_pattern, "http://vocab.getty.edu/page/aat/*", value)
         attr_path = f"{current_path}/@{attr}/{value}"
-        file_occurrences[attr_path] = file_occurrences.get(attr_path, 0) + 1
+        if element.text and element.text.strip():
+            file_occurrences[attr_path] = file_occurrences.get(attr_path, 0) + 1
         if attr_path not in seen_paths:
             if element.text and element.text.strip():
                 seen_paths[attr_path] = {
@@ -79,7 +80,11 @@ def process_xml_folders(folders):
     for path, data in seen_paths.items():
         sample_value = element_samples.get(path, {'value': "N/A", 'filename': "N/A"})['value']
         sample_filename = element_samples.get(path, {'value': "N/A", 'filename': "N/A"})['filename']
-        print(f"{path}\t{sample_value}\t{data['occurrences']}\t{data['max_per_file']}\t{sample_filename}")
+        sample_domain = "digitalcollections" if "digital" in sample_filename else "preserve"
+        sample_url = f"https://{sample_domain}.lib.lehigh.edu/islandora/object/{sample_filename}/dataStream/MODS/download"
+        max_domain = "digitalcollections" if "digital" in data['max_file'] else "preserve"
+        max_url =  f"https://{max_domain}.lib.lehigh.edu/islandora/object/{data['max_file']}/dataStream/MODS/download"
+        print(f"{path}\t{sample_value}\t{sample_filename}\t{sample_url}\t{data['occurrences']}\t{data['max_per_file']}\t{data['max_file']}\t{max_url}")
 
 folders = ['../001-extract-mods/xml/digitalcollections', '../001-extract-mods/xml/preserve']
 process_xml_folders(folders)
